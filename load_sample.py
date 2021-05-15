909f7ce15023cc0ad3f6c8d1985ee5d82fab8883
sh4dow-author <sh4dowauthor@gmail.com>
2021-05-15 20:28:22 +0300

    Now working good, #todo: load big dataset

--- load_sample.py
+++ load_sample.py
@@ -1,11 +1,14 @@
 from CreateTypedefs import send_typedef
 from CreateDatasetEntity import create_dataset
 from CreateSparkProcessEntities import create_process
+from GetNameByGuid import get_guid_by_name
 
 send_typedef("jsons/typedef_datasets.json")  # creating typedef dataset if it not exists
 if create_dataset('zero7zrPricing', 'clean.zero7zrPricing', "my/path", "clean_dataset") == 200:
-    print(f"Dataset zero7zrPricing created")
-if create_dataset('b2b', 'ped.b2b', "my/path", "clean_dataset") == 200:
-    print(f"Dataset b2b created")
+    print(f"Dataset zero7zrPricing created or updated")
 
-# create_process("69237788-b930-4634-b657-ac515d021120","798d2cd9-55ee-40f7-bb59-cbc182c4fb66") you should to go to web UI and get guid but i work for automate this process
+if create_dataset('b2b', 'ped.b2b', "my/path", "clean_dataset") == 200:
+    print(f"Dataset b2b created or updated")
+
+create_process(get_guid_by_name('clean.zero7zrPricing'),
+               get_guid_by_name('ped.b2b'))
