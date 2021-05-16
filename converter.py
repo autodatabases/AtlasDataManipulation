883005ca79545fac5c7eb603e7a05b2c0c290e38
sh4dow-author <sh4dowauthor@gmail.com>
2021-05-16 13:06:37 +0300

    changed structure of relations dataset, and his creating

--- converter.py
+++ converter.py
@@ -7,26 +7,6 @@ def read_df(path):
     return pd.read_csv(path)
 
 
-def create_relations_df(df):
-    data = {'source_dataset': [], 'target_dataset': [], 'task': [], 'path': []}
-    for index, row in df.iterrows():
-        # =====================> Start transforming string representation of list to list
-        source_datasets = eval(row['source_datasets'])
-        target_datasets = eval(row['target_datasets'])
-        # =====================> End transforming
-        # Extracting other parameters
-        path = row['location']
-        task_name = row['task_name']
-
-        for source_dataset in source_datasets:
-            for target_dataset in target_datasets:
-                data['source_dataset'].append(source_dataset)
-                data['target_dataset'].append(target_dataset)
-                data['task'].append(task_name)
-                data['path'].append(path)
-    return pd.DataFrame(data=data)
-
-
 def create_dataset_df(df):
     data = {'name': [], 'qualifiedName': [], 'path': [], 'mark': []}
     for index, row in df.iterrows():
@@ -37,15 +17,16 @@ def create_dataset_df(df):
         # Extracting other parameters
         path = "unknown"
         for source_dataset in source_datasets:
+            # print(source_dataset)
             data['name'].append(source_dataset)
             data['qualifiedName'].append(source_dataset.replace(source_dataset.split('.')[0], '')[1:])
-            data['path'] = path
-            data['mark'] = 'raw_dataset'
+            data['path'].append(path)
+            data['mark'].append('raw_dataset')
         for target_dataset in target_datasets:
             data['name'].append(target_dataset)
             data['qualifiedName'].append(target_dataset.replace(target_dataset.split('.')[0], '')[1:])
-            data['path'] = path
-            data['mark'] = 'clean_dataset'
+            data['path'].append(path)
+            data['mark'].append('clean_dataset')
 
     return pd.DataFrame(data=data)
 
@@ -57,9 +38,8 @@ def export_df_to_csv(df, path):
 def main():
     df_1 = read_df('datalake-ped.csv')
     df_2 = read_df('datalake-cleansing.csv')
-    df = pd.concat([df_1, df_2]).drop_duplicates().reset_index(drop=True)
-    export_df_to_csv(create_relations_df(df), 'relations.csv')
-    # print()
+    df = pd.concat([df_1, df_2]).reset_index()
+    export_df_to_csv(df, 'relations.csv')
     export_df_to_csv(create_dataset_df(df), 'datasets.csv')
 
 
