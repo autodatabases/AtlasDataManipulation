1656347e4f5b8b6b6e5a27a0f3c0a014aa831d3c
sh4dow-author <sh4dowauthor@gmail.com>
2021-05-16 13:08:32 +0300

    changed algorithm of creating processes

--- load_sample.py
+++ load_sample.py
@@ -3,39 +3,46 @@ from CreateDatasetEntity import create_dataset
 from CreateSparkProcessEntities import create_process
 from GetNameByGuid import get_guid_by_name
 import pandas as pd
-import logging
 
 send_typedef("jsons/typedef_datasets.json")  # creating typedef dataset if it not exists
 
 
 def create_entities(df):
+    created = 0
+    all = df.shape[0]
     for ind, row in df.iterrows():
         # print(row['qualifiedName'], row['name'], row['path'], row['mark'])
         if create_dataset(row['qualifiedName'], row['name'], row['path'], row['mark']) == 200:
-            logging.info(f"Dataset {row['name']} created")
+            print(f"Dataset {row['name']} created")
+            created += 1
         else:
-            logging.info(f"Dataset {row['name']} aborted")
+            print(f"Dataset {row['name']} aborted")
+    print(f"{created} / {all}")
 
 
 def create_processes(df):
-    for ind, row in df.iterrows():
-        print(row['target_dataset'], type(row['source_dataset']))
-        # if create_process(get_guid_by_name(row['source_dataset']), get_guid_by_name(row['target_dataset']),
-        #                row['path'], row['task']) == 200:
-        #   print(f"Task {row['task']} created")
+    created = 0
+    all = df.shape[0]
+    for index, row in df.iterrows():
+        # =====================> Start transforming string representation of list to list
+        source_datasets = eval(row['source_datasets'])
+        target_datasets = eval(row['target_datasets'])
+        # =====================> End transforming
+        # Extracting other parameters
+        path = row['location']
+        task_name = row['task_name']
 
-        # else:
-        #    print(f"Task {row['task']} aborted")
-        break
+        for source_dataset in source_datasets:
+            for target_dataset in target_datasets:
+                if create_process(get_guid_by_name(source_dataset), get_guid_by_name(target_dataset), path,
+                                  task_name) == 200:
+                    print(f"Process with name {task_name} was created")
+                    created += 1
+                else:
+                    print(f"Process with name {task_name} wasn't created")
+        print(f"Created {created}/{all}")
 
-# if create_dataset('zero7zrPricing', 'clean.zero7zrPricing', "my/path", "clean_dataset") == 200:
-#    print(f"Dataset zero7zrPricing created or updated")
 
-# if create_dataset('b2b', 'ped.b2b', "my/path", "clean_dataset") == 200:
-#    print(f"Dataset b2b created or updated")
-
-# create_process(get_guid_by_name('clean.zero7zrPricing'),
-#               get_guid_by_name('ped.b2b'))
 if __name__ == "__main__":
-    # create_entities(pd.read_csv('datasets.csv'))
+    create_entities(pd.read_csv('datasets.csv'))
     create_processes(pd.read_csv('relations.csv'))
